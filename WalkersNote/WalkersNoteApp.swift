90019ca658702ab551a9647381d832d3ca631192
0tak2 <0tak2.code@gmail.com>
2025-03-09 09:38:11 +0900

    Initial Commit

---
+++ WalkersNote/WalkersNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkersNoteApp.swift
+//  WalkersNote
+//
+//  Created by 임영택 on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WalkersNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WalkersNoteTests/WalkersNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  WalkersNoteTests.swift
+//  WalkersNoteTests
+//
+//  Created by 임영택 on 3/9/25.
+//
+
+import Testing
+@testable import WalkersNote
+
+struct WalkersNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
